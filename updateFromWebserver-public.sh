645cdcf14ef21ed88a5099cfd16d33910ecaa51a
Anja Christian <caterva@anjachristian.com>
2021-02-01 14:22:40 +0100

    deprecated Scripte geloescht

--- updateFromWebserver-public.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-
-
-# Kopiere Daten auf die Pi
-/home/pi/Git-Clones/webserver-public/Copy2Pi.sh
-
-# Kopiere Daten auf den Business Controller der Caterva
-/home/pi/Git-Clones/webserver-public/Copy2Caterva.sh
