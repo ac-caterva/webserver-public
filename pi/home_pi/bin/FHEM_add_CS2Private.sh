7cc33de38ac75c7acbd9a85fff7e51f84f81ff52
Anja Christian <caterva@anjachristian.com>
2021-09-20 20:57:44 +0200

    2021-09-20

--- pi/home_pi/bin/FHEM_add_CS2Private.sh
+++ pi/home_pi/bin/FHEM_add_CS2Private.sh
@@ -4,9 +4,17 @@ CS_Steuerung_Zeile_FHEM="include ./FHEM/00_CS_Steuerung.cfg"
 
 CS_InPrivtateCfg=`grep ./FHEM/00_CS_Steuerung.cfg /opt/fhem/FHEM/00_Private.cfg | wc -l`
 
+
 if [ ${CS_InPrivtateCfg} = 1 ]; then
     echo "CS_Steuerung war bereits in FHEM integriert. Es gibt nichts zu tun."
 else
+    sudo chmod 664 /opt/fhem/FHEM/00_Private.cfg
     echo $CS_Steuerung_Zeile_FHEM >> /opt/fhem/FHEM/00_Private.cfg
-    echo "CS_Steuerung wurde erfolgreich in FHEM integriert"
+
+    CS_InPrivtateCfg=`grep ./FHEM/00_CS_Steuerung.cfg /opt/fhem/FHEM/00_Private.cfg | wc -l`
+    if [ ${CS_InPrivtateCfg} = 1 ]; then
+        echo "CS_Steuerung wurde erfolgreich in FHEM integriert"
+    else
+        echo "Es gab ein Problem. Bitte wende dich an das Technik Team."  
+    fi      
 fi
