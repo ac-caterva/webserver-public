b083a95be6d26258cbd32cbe09a35d2351e90839
Anja Christian <caterva@anjachristian.com>
2021-10-12 23:03:52 +0200

    2021-10-12

--- Verteilung/CreateTargetDir/CREA_example.sh
+++ Verteilung/CreateTargetDir/CREA_example.sh
@@ -2,7 +2,8 @@
 DIR=/home/admin/foo 
 DIR_USER=fhem
 DIR_GROUP=dialout
-DIR_RIGHTS=654
+DIR_RIGHTS=755
+SUCCESS_COUNTER=0
 
 if ( [ -d $DIR ] ) ; then
     [ $(stat -c '%U' $DIR) != $DIR_USER ]   && sudo chown $DIR_USER $DIR
@@ -13,3 +14,16 @@ else
     sudo chown $DIR_USER:$DIR_GROUP $DIR
     sudo chmod $DIR_RIGHTS $DIR
 fi
+
+if ( [ -d $DIR ] ) ; then
+    [ $(stat -c '%U' $DIR) == $DIR_USER ]   && SUCCESS_COUNTER=`expr $SUCCESS_COUNTER + 1`
+    [ $(stat -c '%G' $DIR) == $DIR_GROUP ]  && SUCCESS_COUNTER=`expr $SUCCESS_COUNTER + 1`
+    [ $(stat -c '%a' $DIR) == $DIR_RIGHTS ] && SUCCESS_COUNTER=`expr $SUCCESS_COUNTER + 1`
+    if [ ${SUCCESS_COUNTER} -eq 3 ] ; then
+        echo "SUCCESS"
+    else
+        echo "NO_SUCCESS"    
+    fi    
+else
+    echo "NO_SUCCESS"   
+fi    
